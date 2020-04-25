bf7e5b54513c73de24eba2488a6f9f5846403f1d
sagar <sagarsean007@gmail.com>
2020-04-26 04:45:24 +0530

    Updates

---
+++ precache-manifest.c66c0e6a6ae33cd6def8e8080464d23c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5a75fd1b3718fbcde150eaea9db51d3",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "588807dc328580f90ba9",
+    "url": "/corona-tracker/static/css/main.b31cd034.chunk.css"
+  },
+  {
+    "revision": "59457031695398723898",
+    "url": "/corona-tracker/static/js/2.cd42362e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.cd42362e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "588807dc328580f90ba9",
+    "url": "/corona-tracker/static/js/main.cb62418c.chunk.js"
+  },
+  {
+    "revision": "4127b310accafee69bc6",
+    "url": "/corona-tracker/static/js/runtime-main.cfc71fe3.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/corona-tracker/static/media/covid_image.d7265326.png"
+  }
+]);
